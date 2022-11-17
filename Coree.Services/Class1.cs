7c1ad8aab17f4e1da002671ac23bd291dd8ff001
Carsten Riedel <carstenriedel@outlook.com>
2022-11-17 21:28:32 +0100

    added services

--- Coree.Services/Class1.cs
+++ Coree.Services/Class1.cs
@@ -1,7 +1,25 @@
-﻿namespace Coree.Services
-{
-    public class Class1
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
 
+namespace Coree.Services
+{
+    public interface IHelloService
+    {
+        string SayHello();
+    }
+
+    public class HelloService : IHelloService
+    {
+        public string SayHello()
+        {
+            return "Hello, world!";
+        }
+    }
+
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection UseHelloService(this IServiceCollection services)
+        {
+            return services.AddScoped<IHelloService, HelloService>();
+        }
     }
 }
